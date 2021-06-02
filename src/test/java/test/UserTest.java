a3b389b9b2831b60c2bceed3d755c4a8353d0dbd
h1542462994 <t1542462994@outlook.com>
2021-06-02 09:51:14 +0800

    update

--- src/test/java/test/UserTest.java
+++ src/test/java/test/UserTest.java
@@ -2,10 +2,7 @@ package test;
 
 import ext.exception.OperationFailedException;
 import ext.exception.ServiceConstructException;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import test.runtime.ActionParser;
@@ -30,17 +27,37 @@ public class UserTest {
         actionParser = container.actionParser();
     }
 
+
+    /**
+     * test Route 1->2->#
+     */
     @Order(1)
+    @DisplayName("testRoute: 1->2->#")
     @ParameterizedTest
-    @CsvFileSource(resources = "csv/test_user_operation_1.csv", numLinesToSkip = 1)
-    public void testAddSuccess(String type, String operation, String arg1, String arg2, String arg3) throws OperationFailedException, FileNotFoundException {
+    @CsvFileSource(resources = "csv/test_user_operation_01.csv", numLinesToSkip = 1)
+    public void test01(String type, String operation, String arg1, String arg2, String arg3) {
         actionParser.dispatch(type, operation, arg1, arg2, arg3);
     }
 
     @Order(2)
+    @DisplayName("testRoute: 1->2->3->#")
     @ParameterizedTest
-    @CsvFileSource(resources = "csv/test_user_operation_2.csv", numLinesToSkip = 1)
-    public void testAddRepeatedly(String type, String operation, String arg1, String arg2, String arg3) {
+    @CsvFileSource(resources = "csv/test_user_operation_02.csv", numLinesToSkip = 1)
+    public void test02(String type, String operation, String arg1, String arg2, String arg3) {
+        actionParser.dispatch(type, operation, arg1, arg2, arg3);
+    }
+
+    @Order(3)
+    @DisplayName("testRoute: 1->2->3->4->#")
+    @ParameterizedTest
+    @CsvFileSource(resources = "csv/test_user_operation_03.csv", numLinesToSkip = 1)
+    public void test03(String type, String operation, String arg1, String arg2, String arg3) {
+        actionParser.dispatch(type, operation, arg1, arg2, arg3);
+    }
+
+    @Order(4)
+    @DisplayName("testRoute: 1->5->true->6->#")
+    public void test04(String type, String operation, String arg1, String arg2, String arg3) {
         actionParser.dispatch(type, operation, arg1, arg2, arg3);
     }
 
